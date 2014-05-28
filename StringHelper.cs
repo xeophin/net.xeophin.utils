74c4ce52b0886b28e5ae2306392cdaa850c23e4d
Kaspar Manz <kaspar.manz@yourehab.com>
2014-05-28 11:36:56 +0200

    Added StringHelper.

---
+++ StringHelper.cs
@@ -0,0 +1,22 @@
+using System;
+
+public static class StringHelper
+{
+  /// <summary>
+  /// Capitalizes the first letter.
+  /// </summary>
+  /// <returns>The string with the first letter capitalised.</returns>
+  /// <param name="s">The string to capitalise.</param>
+  /// 
+  /// <remarks>Based on the code from http://www.dotnetperls.com/uppercase-first-letter.</remarks>
+  public static string CapitalizeFirstLetter (this string s)
+  {
+    if (string.IsNullOrEmpty (s)) {
+      return string.Empty;
+    }
+    char[] a = s.ToCharArray ();
+    a [0] = char.ToUpper (a [0]);
+    return new string (a);
+  }
+}
+
