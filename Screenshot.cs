72ff9f5fbbde8a27df4582717f6cfb99d4b449d0
Kaspar Manz <kaspar.manz@yourehab.com>
2014-05-28 09:39:57 +0200

    Rewrote the code to be more general, added meta files.

--- Screenshot.cs
+++ Screenshot.cs
@@ -1,20 +1,22 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System;
 
-public class Screenshot : MonoBehaviour
+namespace Net.Xeophin.Utils
 {
-  // Use this for initialization
-  void Start ()
+  public class Screenshot : MonoBehaviour
   {
-	
-  }
-  // Update is called once per frame
-  void Update ()
-  {
-    if (Input.GetKeyUp (KeyCode.F1)) {
-      Application.CaptureScreenshot (string.Format ("{0}.png", DateTime.Now.ToString ("yyyyMMdd-HHmmss")), 2);
-      Debug.Log ("Take Screenshot");
+    // Use this for initialization
+    void Start ()
+    {
+    
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+      if (Input.GetKeyUp (KeyCode.F1)) {
+        Application.CaptureScreenshot (string.Format ("{0}.png", DateTime.Now.ToString ("yyyyMMdd-HHmmss")), 2);
+        Debug.Log ("Take Screenshot");
+      }
     }
   }
 }
