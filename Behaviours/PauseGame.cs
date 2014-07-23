dbfc06b48e10c7e458fd74e07bdbb0a341747ac6
Kaspar Manz <kaspar@xeophin.net>
2014-07-23 14:24:47 +0200

    Added PauseGame behaviour.

---
+++ Behaviours/PauseGame.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using SpiroTiger;
+using RespirationTrackerLib;
+using Net.Xeophin.Utils;
+
+public class PauseGame : UnitySingleton<PauseGame>
+{
+  bool isPaused;
+
+  public bool IsPaused {
+    get {
+      return isPaused;
+    }
+    set {
+      if (value && !isPaused) {
+        EventsBroadcaster.Instance.OnPauseGame (new GameStateEventArgs (this));
+        Data.Instance.PauseTraining ();
+      } else if (!value && isPaused) {
+        EventsBroadcaster.Instance.OnUnpauseGame (new GameStateEventArgs (this));
+        Data.Instance.ResumeTraining ();
+      }
+      isPaused = value;
+    }
+  }
+  // Update is called once per frame
+  void Update ()
+  {
+    if (Input.GetKeyUp (KeyCode.Escape)) {
+      IsPaused = !IsPaused;
+    }
+  }
+}
+
