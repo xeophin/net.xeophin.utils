ca0535bd1ca893fa87d1f91868bc71cd6a3519f5
Kaspar Manz <kaspar@xeophin.net>
2014-07-23 15:10:15 +0200

    Easily pause games.

--- Behaviours/PauseGame.cs
+++ Behaviours/PauseGame.cs
@@ -1,33 +1,57 @@
 ﻿using UnityEngine;
-using SpiroTiger;
-using RespirationTrackerLib;
 using Net.Xeophin.Utils;
 
+/// <summary>
+/// Used to Pause/Unpause the game.
+/// </summary>
 public class PauseGame : UnitySingleton<PauseGame>
 {
-  bool isPaused;
+  #region Public data
 
+  /// <summary>
+  /// The keyboard button to use to pause/unpause the game
+  /// </summary>
+  public KeyCode PauseButton = KeyCode.Escape;
+
+
+  /// <summary>
+  /// Gets or sets a value indicating whether this instance is paused.
+  /// </summary>
+  /// <value><c>true</c> if this instance is paused; otherwise, <c>false</c>.</value>
   public bool IsPaused {
     get {
       return isPaused;
     }
     set {
       if (value && !isPaused) {
-        EventsBroadcaster.Instance.OnPauseGame (new GameStateEventArgs (this));
-        Data.Instance.PauseTraining ();
+        EventsBroadcaster.Instance.RaiseGameStateChanged (this, new GameStateEventArgs (GameState.EnterPause));
       } else if (!value && isPaused) {
-        EventsBroadcaster.Instance.OnUnpauseGame (new GameStateEventArgs (this));
-        Data.Instance.ResumeTraining ();
+        EventsBroadcaster.Instance.RaiseGameStateChanged (this, new GameStateEventArgs (GameState.ExitPause));
       }
       isPaused = value;
     }
   }
-  // Update is called once per frame
+
+  #endregion
+
+  #region Private data
+
+  bool isPaused;
+
+  #endregion
+
+  #region MonoBehaviour
+
+  /// <summary>
+  /// Checks whether the pause button has been pressed.
+  /// </summary>
   void Update ()
   {
-    if (Input.GetKeyUp (KeyCode.Escape)) {
+    if (Input.GetKeyUp (PauseButton)) {
       IsPaused = !IsPaused;
     }
   }
+
+  #endregion
 }
 
