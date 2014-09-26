bbd345510357d38c1685ec3126e7b4bf49b15935
Kaspar Manz <kaspar.manz@yourehab.com>
2014-09-26 12:10:13 +0200

    Updated the game events broadcaster to be more flexible with events
    * Removed the ugly switch statement
    * Handlers are now managed in a dictionary
    * Added the first tests (stub)

---
+++ Editor/Test/EventsBroadcasterTest.cs
@@ -0,0 +1,33 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using Net.Xeophin.Utils;
+using System;
+
+/// <summary>
+/// Events broadcaster tests. Currently not working, more to come.
+/// </summary>
+[TestFixture]
+public class EventsBroadcasterTest
+{
+  [Test]
+  public void TestFadeIn ()
+  {
+    EventsBroadcaster geb = new EventsBroadcaster ();
+    bool wasCalled = false;
+
+    geb.ReadyForFadeIn += (sender, e) => wasCalled = true;
+
+    geb.RaiseGameStateChanged (this, new GameStateEventArgs (GameState.ReadyForFadeIn));
+    Assert.That (wasCalled);
+  }
+
+
+  [Test]
+  [ExpectedException]
+  public void WrongEnum ()
+  {
+    EventsBroadcaster geb = new EventsBroadcaster ();
+
+    geb.RaiseGameStateChanged (this, new GameStateEventArgs (Enum.Parse (typeof(GameState), "Blubdiblub")));
+  }
+}
